15449aefb5dbf38d0e591b319006946c53b5bf30
anjuit <anjuramanbe@gmail.com>
2016-08-13 11:00:32 +0530

    Update Revnum.java

--- Revnum.java
+++ Revnum.java
@@ -11,7 +11,8 @@ class Revnum
       System.out.println("Enter the number to reverse");
       Scanner in = new Scanner(System.in);
       n = in.nextInt();
- 
+    if(n>0)
+    {
       while( n != 0 )
       {
           reverse = reverse * 10;
@@ -20,5 +21,8 @@ class Revnum
       }
  
       System.out.println("Reverse of entered number is "+reverse);
+    }
+    else
+    System.out.println("number should be greater than 0");
    }
 }
