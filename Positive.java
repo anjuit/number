b47104b8072e1874aac1311f1f7cb64c3380b490
anjuit <anjuramanbe@gmail.com>
2016-08-13 10:07:48 +0530

    Update and rename .gitignore to Positive.java

---
+++ Positive.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Positive
+{
+public static void main(String ag[])
+{
+Scanner sc=new Sacnner (System.in);
+int number=sc.nextInt();
+if(number>0)
+{
+System.out.println("positive");
+}
+ else if(number<0)
+{
+System.out.println("positive");
+}
+else
+{
+System.out.println("0");
+}
+}
+}
